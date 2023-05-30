86372315d95a419be0dbe22c8b6cea05f71dd114
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-05-30 21:30:46 +0300

    error handling updated

--- TaskManager/app.js
+++ TaskManager/app.js
@@ -4,6 +4,8 @@ const PORT = 5000
 require("dotenv").config()
 const MONGO_URI = process.env.MONGO_URI
 
+const notFound = require("./middleware/not-found");
+const errorHandleMiddleware = require("./middleware/error-handler");
 
 //db import
 const connectDB =require("./db/connect");
@@ -15,14 +17,15 @@ const tasks = require("./routes/tasks");
 //middleware
 app.use(express.json())
 
+
 //routes
 app.get("/hello", (req, res) => {
     res.send("Hello World").status(200)
 });
 
 app.use("/api/v1/tasks", tasks)
-
-
+app.use(notFound);
+app.use(errorHandleMiddleware);
 
 const start = async () => {
     try {
