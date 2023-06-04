ac6377bed9a77031148b999ac5503d20def6bb91
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-06-04 15:42:32 +0300

    store api finalized

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 
 
 const getAllProducts = async (req, res) => {
-    const {name, company, featured, sort} = req.query
+    const {name, company, featured, sort, fields, numericFilters} = req.query
 
     const queryObject = {}
 
@@ -17,7 +17,37 @@ const getAllProducts = async (req, res) => {
     if(name) {
         queryObject.name = { $regex: name, $options: 'i' }
     }
+
+    //Adding Numberic Filters
+    if(numericFilters) {
+        const operatorMap = {
+            ">": "$gt",
+            ">=": "$gte",
+            "=": "$eq",
+            "<": "$lt",
+            "<=": "$lte"
+        }
+        
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        const filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+        //console.log(filters)
+
+        const options = ["price", "rating"]
+
+        filters = filters.split(",").forEach((item) => {
+            const [field, operator,value] = item.split("-")        
+            if(options.includes(field)){
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        })
+
+        //console.log(numericFilters)
+    }
+
+
+
     console.log(queryObject);
+
     let result = Product.find(queryObject)
 
     if(sort){
@@ -27,9 +57,24 @@ const getAllProducts = async (req, res) => {
     } else {
         result = result.sort('createdAt')
     }
+
+    if(fields) {
+        const fieldsList = fields.split(",").join(' ')
+        result = result.select(fieldsList);
+        console.log(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit);
+
+
     const products = await result
     res.status(200).json({products, count: products.length})
 }
+
+
 
 const createProduct = async(req, res) => {
     res.status(201).json({msg: "product created successfully!!"})
